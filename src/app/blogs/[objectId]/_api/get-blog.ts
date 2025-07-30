a73d0cce6fb166ac38006385f2613c48fb47dd92
pramadito <pramadito@gmail.com>
2025-07-30 15:19:49 +0700

    feat: slug

--- src/app/blogs/[objectId]/_api/get-blog.ts
+++
@@ -1,8 +0,0 @@
-import { axiosInstance } from "@/lib/axios";
-import { Blog } from "@/types/blog";
-import { cache } from "react";
-
-export const getBlog = cache(async (objectId: string) => {
-  const { data } = await axiosInstance.get<Blog>(`/api/data/Blogs/${objectId}`);
-  return data;
-});
